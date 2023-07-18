becc58b25885e97fe8cae08ecbccd406d5a42535
henriquefsa98 <60986807+henriquefsa98@users.noreply.github.com>
2023-07-18 16:42:43 +0000

    criacao da pagina do mapa interativo

---
+++ src/mapa.ts
@@ -0,0 +1,47 @@
+function apontador(){
+
+    const description = document.querySelector(".tooltip");
+
+    if(description instanceof HTMLElement){
+    
+        document.querySelectorAll('path').forEach((el) =>
+            el.addEventListener('mouseover', (event) => {
+                if(event.target instanceof Element){
+                    //event.target.className = ("enabled");
+                    //event.target.setAttribute("class", "enabled");//faz com que os estados visitados fiquem azul...
+                    description.classList.add("active");
+                    let conteudotxt = event.target.getAttribute("class") || ""
+                    if (conteudotxt.length > 2){
+                        conteudotxt = conteudotxt[0].toUpperCase() + conteudotxt.slice(1);
+                    }
+                    description.innerHTML = conteudotxt;
+                    //console.log("mouseover");
+                }
+            })
+
+        );
+
+        document.querySelectorAll('path').forEach((el) =>
+            el.addEventListener("mouseout", () => {
+                description.classList.remove("active");
+                //console.log("mouseout");
+            })
+        );
+
+        document.onmousemove = function (e) {
+            
+            description.style.left = e.pageX + "px";
+            description.style.top = (e.pageY - 70) + "px";
+            //console.log("mouse move");
+            
+        }
+    }
+
+}
+
+
+function main(){
+    apontador();
+}
+
+main();
