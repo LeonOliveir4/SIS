a4b20885cde4eea8fdcad5f4b391407c79dc3984
Leonardo Pires de Oliveira <77010123+leonardo-Pires-de-Oliveira@users.noreply.github.com>
2023-08-20 16:46:07 +0000

    adicionando comandos para rodar server e para compilar apenas o mapa.ts para gerar o js da logica do mapa interativo

--- dist/mapa.js
+++ dist/mapa.js
@@ -1,36 +1,31 @@
+"use strict";
 function apontador() {
-    var description = document.querySelector(".tooltip");
+    const description = document.querySelector(".tooltip");
     if (description instanceof HTMLElement) {
-        document.querySelectorAll('.regiao').forEach(function (el) {
-            return el.addEventListener('mouseover', function (event) {
-                if (event.target instanceof Element) {
-                    //event.target.className = ("enabled");
-                    //event.target.setAttribute("class", "enabled");//faz com que os estados visitados fiquem azul...
-                    description.classList.add("active");
-                    var conteudotxt = event.target.getAttribute("class") || "";
-                    if (conteudotxt.length > 2) {
-                        conteudotxt = conteudotxt[0].toUpperCase() + conteudotxt.slice(1);
-                    }
-                    description.innerHTML = conteudotxt;
-                    //console.log("mouseover");
+        document.querySelectorAll('.regiao').forEach((el) => el.addEventListener('mouseover', (event) => {
+            if (event.target instanceof Element) {
+                //event.target.className = ("enabled");
+                //event.target.setAttribute("class", "enabled");//faz com que os estados visitados fiquem azul...
+                description.classList.add("active");
+                let conteudotxt = event.target.getAttribute("class") || "";
+                if (conteudotxt.length > 2) {
+                    conteudotxt = conteudotxt[0].toUpperCase() + conteudotxt.slice(1);
                 }
-            });
-        });
-        document.querySelectorAll('.regiao').forEach(function (el) {
-            return el.addEventListener("mouseout", function () {
-                description.classList.remove("active");
-                //console.log("mouseout");
-            });
-        });
-        document.querySelectorAll('.regiao').forEach(function (el) {
-            return el.addEventListener("click", function (event) {
-                //description.classList.remove("active");
-                console.log("clickou");
-                if (event.target instanceof Element) {
-                    window.location.href = 'regiao?regiao=' + event.target.getAttribute("class");
-                }
-            });
-        });
+                description.innerHTML = conteudotxt;
+                //console.log("mouseover");
+            }
+        }));
+        document.querySelectorAll('.regiao').forEach((el) => el.addEventListener("mouseout", () => {
+            description.classList.remove("active");
+            //console.log("mouseout");
+        }));
+        document.querySelectorAll('.regiao').forEach((el) => el.addEventListener("click", (event) => {
+            //description.classList.remove("active");
+            console.log("clickou");
+            if (event.target instanceof Element) {
+                window.location.href = 'regiao?regiao=' + event.target.getAttribute("class");
+            }
+        }));
         document.onmousemove = function (e) {
             description.style.left = e.pageX + "px";
             description.style.top = (e.pageY - 70) + "px";
