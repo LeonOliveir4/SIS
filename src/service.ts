d0071f6b835f4f2dde15deb1169960d8db8c2a51
henriquefsa98 <60986807+henriquefsa98@users.noreply.github.com>
2023-08-23 18:17:03 +0000

    database service finished

---
+++ src/service.ts
@@ -0,0 +1,89 @@
+import {Database} from './database'
+import {DoencaItem, DoencaItemDAO} from './model'
+
+/**
+ * Custom exception to signal a database error
+ */
+export class DatabaseError extends Error {}
+
+/**
+ * A service to perform CRUD operations over a DoencaItem
+ */
+export class DoencaItemService {
+    dao: DoencaItemDAO
+
+    /**
+     * The constructor.
+     *
+     * @param {Database} database the database
+     */
+    constructor(database: Database) {
+        this.dao = new DoencaItemDAO(database)
+    }
+
+    /**
+     * List the items
+     *
+     * @return {DoencaItem[]} a list of Doenca items
+     */
+    async list(): Promise<DoencaItem[]> {
+        return await this.dao.list()
+    }
+
+    /**
+     * Add a new item
+     *
+     * @param {any} json the JSON representation of the item to add
+     */
+    async add(json: any): Promise<void> {
+        const status = await this.dao.insert(DoencaItem.fromJSON(json))
+
+        if (!status) {
+            throw new DatabaseError('Failed to insert item in the database')
+        }
+    }
+
+    /**
+     * Remove an item
+     *
+     * @param {number} id the id of the item to remove
+     */
+    async remove(id: number): Promise<void> {
+        const status = await this.dao.removeById(id)
+
+        if (!status) {
+            throw new DatabaseError(`Failed to remove item ${id} from
+                                                         the database`);
+        }
+    }
+
+    /**
+     * Add a new item
+     *
+     * @param {any} json the JSON representation of the item to add
+     */
+    async update(json: any): Promise<void> {
+        const status = await this.dao.update(DoencaItem.fromJSON(json));
+
+        if (!status) {
+            throw new DatabaseError('Failed to insert item in the database')
+        }
+    }
+
+    /**
+     * Get an item
+     *
+     * @param {number} id the id of the item to get
+     * @return {DoencaItem} a DoencaItem
+     */
+    async get(id: number): Promise<DoencaItem> {
+        const item = await this.dao.findById(id)
+
+        if (!item) {
+            throw new DatabaseError(`Failed to find item ${id} from
+                                                         the database`);
+        }
+
+        return item;
+    }
+}
